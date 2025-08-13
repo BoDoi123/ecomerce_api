fc0cdc02073c0e9cfd1615d81ee82033a1b71a8a
Chu Long <chulong_t65@hus.edu.vn>
2025-08-13 13:53:53 +0700

    Viết API cho delete và lấy thông tin người dùng

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,7 +3,6 @@ const UserService = require("../services/UserService");
 const createUser = async (req, res) => {
 	try {
 		const { name, email, password, confirmPassword, phone } = req.body;
-
 		const reg = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/;
 		const isCheckEmail = reg.test(email);
 
@@ -85,8 +84,60 @@ const updateUser = async (req, res) => {
 	}
 };
 
+const deleteUser = async (req, res) => {
+	try {
+		const userId = req.params.id;
+		if (!userId) {
+			return res.status(200).json({
+				status: "ERR",
+				message: "The userId is required",
+			});
+		}
+
+		const response = await UserService.deleteUser(userId);
+		return res.status(200).json(response);
+	} catch (e) {
+		return res.status(404).json({
+			message: e,
+		});
+	}
+};
+
+const getAllUser = async (req, res) => {
+	try {
+		const response = await UserService.getAllUser();
+		return res.status(200).json(response);
+	} catch (e) {
+		return res.status(404).json({
+			message: e,
+		});
+	}
+};
+
+const getDetailsUser = async (req, res) => {
+	try {
+		const userId = req.params.id;
+		if (!userId) {
+			return res.status(200).json({
+				status: "ERR",
+				message: "The userId is required",
+			});
+		}
+
+		const response = await UserService.getDetailsUser(userId);
+		return res.status(200).json(response);
+	} catch (e) {
+		return res.status(404).json({
+			message: e,
+		});
+	}
+};
+
 module.exports = {
 	createUser,
 	loginUser,
 	updateUser,
+	deleteUser,
+	getAllUser,
+	getDetailsUser,
 };
